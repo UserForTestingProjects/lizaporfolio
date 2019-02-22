334fccc6b7d4228a9991b7a63dc5cd4d977cbf7d
Павел Бабанов <pavelbabanov@Pavel-Babanovs-MacBook-Pro.local>
2019-02-22 15:55:44 +0300

    Updates

---
+++ precache-manifest.0d798b10ca8e06083cd43e2ac8941b17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecf44d2b092d09fee118a5c10dd14d2a",
+    "url": "/lizaporfolio/static/media/resum.ecf44d2b.jpg"
+  },
+  {
+    "revision": "02ac7cd244d715df28e9349b60b0885d",
+    "url": "/lizaporfolio/static/media/face.02ac7cd2.svg"
+  },
+  {
+    "revision": "7061ef6ea76f78bf988426efa3387238",
+    "url": "/lizaporfolio/static/media/cont.7061ef6e.jpg"
+  },
+  {
+    "revision": "445b69fa0737b0e2be72",
+    "url": "/lizaporfolio/static/js/runtime~main.445b69fa.js"
+  },
+  {
+    "revision": "caf47da534310b15e148",
+    "url": "/lizaporfolio/static/js/main.caf47da5.chunk.js"
+  },
+  {
+    "revision": "bcaf39ec74bbacda4881",
+    "url": "/lizaporfolio/static/js/2.bcaf39ec.chunk.js"
+  },
+  {
+    "revision": "caf47da534310b15e148",
+    "url": "/lizaporfolio/static/css/main.27f8c514.chunk.css"
+  },
+  {
+    "revision": "bcaf39ec74bbacda4881",
+    "url": "/lizaporfolio/static/css/2.b996ce2b.chunk.css"
+  },
+  {
+    "revision": "f74661e4833fd46ed45b3a0116302587",
+    "url": "/lizaporfolio/index.html"
+  }
+];
